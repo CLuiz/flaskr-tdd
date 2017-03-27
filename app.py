40db173e2fafcaba91a1ad2dbae3be9bfec096d2
Chris Luiz <chris@chrisluiz.io>
2017-03-27 17:01:21 -0600

    more tests, more templates

--- app.py
+++ app.py
@@ -17,12 +17,37 @@ app.config.from_object(__name__)
 
 
 @app.route('/')
-def show_entries():
+def index():
     """Searches the db for entries, then displays them"""
     db = get_db()
     cur = db.execute('SELECT * FROM entries ORDER BY id DESC')
     entries = cur.fetchall()
     return render_template('index.html', entries=entries)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management"""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    """User logout/authentiation/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
 
 # connect to db
 def connect_db():
